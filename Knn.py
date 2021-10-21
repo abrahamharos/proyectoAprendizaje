525ada34685f227f5982acf661ab05d148fe5947
DiegoPerezVega <A01329098@itesm.mx>
2021-10-20 23:22:41 -0500

    Se inserta codigo de KNN para casos 3.1 y 4.1

---
+++ Knn.py
@@ -0,0 +1,47 @@
+import load_dataset as ld
+from sklearn.neighbors import KNeighborsClassifier
+
+XTrain, XTest, yTrain, yTest = ld.load_dataset('3.1')
+
+n_neighbors = [1, 2, 3, 5, 10, 15, 20, 50, 75, 100]
+values = []
+maxModelScore, maxModelIndex = 0, 0
+maxModel = {}
+
+for i in range(len(n_neighbors)):
+    modelTemp = KNeighborsClassifier(n_neighbors=n_neighbors[i], algorithm='brute')
+    modelTemp.fit(XTrain, yTrain)
+    Y_predTemp = modelTemp.predict(XTest)
+    values.append(float(modelTemp.score(XTest, yTest)))
+    print("N-Neighbors: {} - Score: {}".format(n_neighbors[i],values[-1]))
+    if values[-1] > maxModelScore:
+        maxModelScore = values[-1]
+        maxModelIndex = i
+        maxModel = Y_predTemp
+
+print("\nCase 3.1")
+print("N-Neighbors: {}".format(n_neighbors[maxModelIndex]))
+print("Accuracy: {}\n".format(maxModelScore))
+
+
+XTrain, XTest, yTrain, yTest = ld.load_dataset('4.1')
+
+n_neighbors = [1, 2, 3, 5, 10, 15, 20, 50, 75, 100]
+values = []
+maxModelScore, maxModelIndex = 0, 0
+maxModel = {}
+
+for i in range(len(n_neighbors)):
+    modelTemp = KNeighborsClassifier(n_neighbors=n_neighbors[i], algorithm='brute')
+    modelTemp.fit(XTrain, yTrain)
+    Y_predTemp = modelTemp.predict(XTest)
+    values.append(float(modelTemp.score(XTest, yTest)))
+    print("N-Neighbors: {} - Score: {}".format(n_neighbors[i],values[-1]))
+    if values[-1] > maxModelScore:
+        maxModelScore = values[-1]
+        maxModelIndex = i
+        maxModel = Y_predTemp
+
+print("\nCase 4.1")
+print("N-Neighbors: {}".format(n_neighbors[maxModelIndex]))
+print("Accuracy: {}".format(maxModelScore))
