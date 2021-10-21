19531f21c73acce9d4347599bdd0a388c3015fcd
Andyrg99 <31376019+Andyrg99@users.noreply.github.com>
2021-10-20 19:35:28 -0500

    2.1 cases added

--- dataset/scripts/split_dataset.py
+++ dataset/scripts/split_dataset.py
@@ -58,9 +58,38 @@ np.savetxt('../casos/1.3/yTest.csv', yTest)
 print(" ~ Caso 1.3 Guardado en ../casos/1.3/")
 
 # Caso 2.1: Predecir CO2 dados km/l en carretera, potencia, modelo y cilindros.
+X1 = data.iloc[:, 3].values.reshape(-1, 1) # Modelo
+X2 = data.iloc[:, 6:8].values.reshape(-1, 2) # Cilindros y potencias
+X3 = data.iloc[:, 11].values.reshape(-1, 1) # km/l en carretera
+X = np.append(X1, X2,1) # [MODELO, CILINDROS, AÑO, KM/L EN CARRETERA]
+X = np.append(X, X3,1) # [MODELO, CILINDROS, AÑO, KM/L EN CARRETERA]
+
+y = data.iloc[:, 14].values.reshape(-1, 1) # CO2
+
+XTrain, XTest, yTrain, yTest = train_test_split(X, y, test_size=0.2)
+
+np.savetxt('../casos/2.1/XTrain.csv', XTrain)
+np.savetxt('../casos/2.1/XTest.csv', XTest)
+np.savetxt('../casos/2.1/yTrain.csv', yTrain)
+np.savetxt('../casos/2.1/yTest.csv', yTest)
+print(" ~ Caso 2.1 Guardado en ../casos/2.1/")
 
 # Caso 2.2: Predecir CO2 dados km/l en ciudad, potencia, modelo y cilindros.
+X1 = data.iloc[:, 3].values.reshape(-1, 1) # Modelo
+X2 = data.iloc[:, 6:8].values.reshape(-1, 2) # Cilindros y potencias
+X3 = data.iloc[:, 10].values.reshape(-1, 1) # km/l en ciudad
+X = np.append(X1, X2,1) # [MODELO, CILINDROS, AÑO, KM/L EN CIUDAD]
+X = np.append(X, X3,1) # [MODELO, CILINDROS, AÑO, KM/L EN CIUDAD]
 
+y = data.iloc[:, 14].values.reshape(-1, 1) # CO2
+
+XTrain, XTest, yTrain, yTest = train_test_split(X, y, test_size=0.2)
+
+np.savetxt('../casos/2.2/XTrain.csv', XTrain)
+np.savetxt('../casos/2.2/XTest.csv', XTest)
+np.savetxt('../casos/2.2/yTrain.csv', yTrain)
+np.savetxt('../casos/2.2/yTest.csv', yTest)
+print(" ~ Caso 2.2 Guardado en ../casos/2.2/")
 # Caso 2.3: Predecir CO2 dados km/l combinados (ciudad y carretera), potencia, modelo y cilindros.
 
 # Caso 3.1: Clasificar la instancia por calificación de contaminación del aire
